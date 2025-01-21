ee18cf2f60f2291f8a924cfcf574ad7a8fc8d3b4
Milla-J <milla.jerohin@vuoksi.fi>
2025-01-21 13:25:57 +0200

    Animation added

---
+++ Assets/Scripts/AnimationTest.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour
+{
+    [SerializeField] Animator animator1;
+    [SerializeField] Animator animator2;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TestAnimation1();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            TestAnimation2();
+        }
+    }
+
+    private void TestAnimation1()
+    {
+        animator1.SetBool("IsBroken", true);
+    }
+
+    private void TestAnimation2()
+    {
+        animator1.SetBool("IsBroken", true);
+    }
+}
